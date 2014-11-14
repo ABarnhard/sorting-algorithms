38a84f7ce9048e3b4e2c38bebd01669aa9c23ccb
Adam Barnhard <admbarn@gmail.com>
2014-11-14 14:19:45 -0600

    added recursive selection sort

--- selection.py
+++ selection.py
@@ -11,4 +11,19 @@ selection_sort(nums)
 
 print(nums)
 
+
+def recursive_sel_sort(array, out_array=None):
+    if not out_array:
+        out_array = []
+    if not array:
+        return out_array
+    min_val = min(array)
+    array.remove(min_val)
+    out_array.append(min_val)
+    return recursive_sel_sort(array, out_array)
+
+nums2 = [20, 3, 7, 56, 2, 77, 100, 43, 698, 1]
+sorted_nums = recursive_sel_sort(nums2)
+print(sorted_nums)
+
 pass
